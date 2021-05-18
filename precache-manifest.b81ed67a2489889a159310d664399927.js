69cafeb0a99e4d08c6c9aed2cc2dc8bd430b0640
RamanSlinka <romuch1986@gmail.com>
2021-05-19 00:18:00 +0300

    Updates

---
+++ precache-manifest.b81ed67a2489889a159310d664399927.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fea55d5d0170a715bbe5cacdaa3b45ce",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "478ea616d0ca944dab9f",
+    "url": "/TodoList/static/css/main.63e731dd.chunk.css"
+  },
+  {
+    "revision": "b1e8e9f90478c168be10",
+    "url": "/TodoList/static/js/2.ef26292b.chunk.js"
+  },
+  {
+    "revision": "d2266803016540034565984c8bb59069",
+    "url": "/TodoList/static/js/2.ef26292b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "478ea616d0ca944dab9f",
+    "url": "/TodoList/static/js/main.f8108f8b.chunk.js"
+  },
+  {
+    "revision": "6f4002f704c746794b97",
+    "url": "/TodoList/static/js/runtime-main.b918c17e.js"
+  }
+]);
